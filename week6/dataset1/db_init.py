5c73b6a706c83206ee597c4eed816a10d954bcda
Kedar-V <kedarvaidya0504@gmail.com>
2025-10-08 01:43:13 +0000

    used a dataset with 20columns

--- week6/dataset1/db_init.py
+++ week6/dataset1/db_init.py
@@ -2,10 +2,10 @@ import pandas as pd
 import sqlite3
 from pathlib import Path
 
-csv_path = "2011_rankings.csv" 
+csv_path = "ecommerce_data.csv" 
 db_path = Path(csv_path).with_suffix(".db")
 
 df = pd.read_csv(csv_path)
 conn = sqlite3.connect(db_path)
-df.to_sql("university_rankings", conn, if_exists="replace", index=False)
+df.to_sql("sales_data", conn, if_exists="replace", index=False)
 conn.close()
